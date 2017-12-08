98242d081ae2bd0b6c2867c6579bc8ff1c76832b
y-kurami <yosuke.kurami@gmail.com>
2017-12-09 04:23:04 +0900

    refactor

--- front/assets/worker.js
+++ front/assets/worker.js
@@ -2,25 +2,17 @@ importScripts('wasm-util.js', 'module.js', '../built/cv-wasm.js');
 
 function mat2Image(mat) {
   const width = mat.cols, height = mat.rows, channels = mat.channels(), data = new Uint8Array(mat.data());
-  return {
-    width,
-    height,
-    channels,
-    data,
-  };
+  return { width, height, channels, data };
 }
 
-function detectFace(input, roi) {
-    const classfier = new cv.CascadeClassifier();
-    classfier.load('../../test/data/haarcascade_frontalface_default.xml');
-    const img = cv.matFromArray(input, 24);
+function detectFace(inputImage) {
+    const classifier = new cv.CascadeClassifier();
+    classifier.load('../../test/data/haarcascade_frontalface_default.xml');
+    const imgRaw = cv.matFromArray(inputImage, cv.CV_8UC4);
     const imgColor = new cv.Mat();
-    cv.cvtColor(img, imgColor, cv.ColorConversionCodes.COLOR_RGBA2BGR.value, 0);
+    cv.cvtColor(imgRaw, imgColor, cv.ColorConversionCodes.COLOR_RGBA2BGR.value, 0);
     const faces = new cv.RectVector();
-    const s1 = [0, 0];
-    const s2 = [0, 0];
-    classfier.detectMultiScale(imgColor, faces, 1.1, 3, 0, s1, s2);
-    console.log(faces);
+    classifier.detectMultiScale(imgColor, faces, 1.1, 3, 0, [0, 0], [0, 0]);
 
     let x;
     if (!faces.size()) {
@@ -30,13 +22,11 @@ function detectFace(input, roi) {
       console.log(rect.x, rect.y, rect.width, rect.height);
       x = imgColor.getROI_Rect(rect);
     }
+
     const resized = new cv.Mat();
     cv.resize(x, resized, [28, 28], 0, 0, 0);
     const out = mat2Image(resized);
-    img.delete();
-    imgColor.delete();
-    faces.delete();
-    classfier.delete();
+    [imgRaw, imgColor, faces, classifier, resized].forEach(p => p.delete())
     return out;
 }
 
